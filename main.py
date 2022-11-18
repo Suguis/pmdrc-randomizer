a3cea71faa25ea37799beba51839514b2ec8e644
Suguivy <anaisusg@gmail.com>
2022-11-18 12:14:05 +0100

    Added gui to load file and rom size verification

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import os
 import sys
 import json
-
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 from randomize import randomize
 
+ORIGINAL_ROM_SIZE = 33554432
+
 def main():
-    if len(sys.argv) >= 2:
-        rom_path = sys.argv[1]
+    root = tk.Tk()
+    root.withdraw()
+    rom_path = filedialog.askopenfilename()
+    try:
+        size = os.path.getsize(rom_path)
+        if size != ORIGINAL_ROM_SIZE:
+            raise Exception("The selected file size doesn't match the expected rom file size")
         with open('config.json') as config_json:
             config = json.load(config_json)
         randomize(rom_path, config)
-    else:
-        print("Rom not provided")
+    except Exception as e:
+        messagebox.showerror("Error", e)
 
 if __name__ == "__main__":
     main()
