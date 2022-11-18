cfd4336bb71c0cf015e4d92e1ceb2675a6d948de
Suguivy <anaisusg@gmail.com>
2022-11-18 11:55:47 +0100

    improved build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-pyinstaller --onefile --console main.py
+pyinstaller -n pmdrc-randomizer --onefile --console main.py
+mkdir -p "release/$OSTYPE"
+zip -j "$OSTYPE.zip" "./dist/main" config.json
